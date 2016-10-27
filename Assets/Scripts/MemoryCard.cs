3ddf357a1470b6eabd7e94dd46b4c41ffb9a0727
Ruslan Durov <s.t.o.k.a.t.p@gmail.com>
2016-10-27 16:52:38 +0300

    Complete

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MemoryCard : MonoBehaviour {
+
+    [SerializeField]
+    private SceneController controller;
+
+    [SerializeField]
+    private GameObject cardBack;
+
+    private int _id;
+    public int id
+    {
+        get { return _id; }
+    }
+
+    public void SetCard(int id , Sprite image)
+    {
+        _id = id;
+
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+
+    public void OnMouseDown()
+    {
+        if(cardBack.activeSelf && controller.canReveal) 
+        {
+            cardBack.SetActive(false); // Деактивирует объект, делая его невидимым
+            controller.CardRevealed(this);
+        }
+    }
+
+    public void Unreveal()
+    {
+        cardBack.SetActive(true);
+    }
+
+}
---
+++ Assets/Scripts/UIButton.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIButton : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject targetObject;
+    [SerializeField]
+    private string targetMessage;
+
+    public Color highlightColor = Color.cyan;
+
+    public void OnMouseOver()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if(sprite != null)
+        {
+            sprite.color = highlightColor;
+        }
+    }
+
+    public void OnMouseExit()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if(sprite != null)
+        {
+            sprite.color = Color.white;
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        transform.localScale = new Vector3(1.1f, 1.1f, 1.1f); // Слегка увеличиваем при щелчке
+    }
+
+    public void OnMouseUp()
+    {
+        transform.localScale = Vector3.one;
+        if(targetObject != null)
+        {
+            targetObject.SendMessage(targetMessage); // Отправляем сообщение при щелчке по кнопке
+        }
+    }
+
+}
